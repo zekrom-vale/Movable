e38c64fa62098c1b2f8e3fe49dddc795b8424a89
Shawn <sgraven66@gmail.com>
2019-11-02 14:41:39 -0500

    Added comments

--- src/movable/ResizableCircle.java
+++ src/movable/ResizableCircle.java
@@ -2,8 +2,14 @@ package movable;
 
 import java.awt.Color;
 
+/**
+* Class ResizableCircle, extends Circle (implimenting Movable) and impliments Resizable
+*/
+
 @SuppressWarnings({"javadoc", "hiding"})
 public class ResizableCircle extends Circle implements Resizable{
+	//No need to impliment Movable as the subclass already does
+	//Plus extending Circle already comes with the Movable imliminted methods so no need to define them here!
 	public ResizableCircle(
 		final int x, final int y, final double dx, final double dy,
 		final int radius, final Color color
